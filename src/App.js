675d0bcb676069ae5f70bce9f4d864a85346bd60
sunnyvucs <sunny.vucs@gmail.com>
2025-03-09 20:57:44 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import CustomerPage from './CustomerPage';
+
+function App() {
+  return (
+    <div className="App">
+      <CustomerPage />
+    </div>
+  );
+}
+
+export default App;
